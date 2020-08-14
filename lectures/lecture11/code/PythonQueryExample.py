4bb3ac3dc18c26a5fdaf02896b89ae3c6d2f8566
Ramon Lawrence <ramon.lawrence@ubc.ca>
2020-08-14 11:10:11 -0700

    Update PythonQueryExample.py

--- lectures/lecture11/code/PythonQueryExample.py
+++ lectures/lecture11/code/PythonQueryExample.py
@@ -1,6 +1,6 @@
 import mysql.connector
 try:
-    cnx = mysql.connector.connect(user='rlawrenc', password='test', host='cosc304.ok.ubc.ca', database='workson', ssl_disabled='True')
+    cnx = mysql.connector.connect(user='rlawrenc', password='todo', host='cosc304.ok.ubc.ca', database='workson', ssl_disabled='True')
     cursor = cnx.cursor()
     query = "SELECT ename, salary FROM emp"                
     cursor.execute(query)
