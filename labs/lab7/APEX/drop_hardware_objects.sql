e225bbc2b917f0801685307753f8c9188e85109a
Youry <ykhmelevsky@okanagan.bc.ca>
2022-06-09 00:02:50 -0700

    Lab 7 sql scripts

---
+++ labs/lab7/APEX/drop_hardware_objects.sql
@@ -0,0 +1,9 @@
+Rem  Copyright � 2020 Oracle and/or its affiliates.  All rights reserved.
+Rem  Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl. 
+
+
+drop SEQUENCE DEPARTMENT_LOOKUP_SEQ;
+drop sequence HARDWARE_SEQ1;  
+drop table DEPARTMENT_LOOKUP cascade constraints;
+drop table HARDWARE cascade constraints;
+DROP PROCEDURE HARDWARE_DATA;
