cfdb19d84eb953e76ba7f0f0d06a768d315c2582
Ramon Lawrence <ramon.lawrence@ubc.ca>
2022-11-08 08:29:51 -0800

    Update JdbcTransactions.java

--- labs/lab8/code/JdbcTransactions.java
+++ labs/lab8/code/JdbcTransactions.java
@@ -13,7 +13,7 @@ import java.sql.*;
 
 public class JdbcTransactions
 {	public static void main(String[] args)
-	{	String url = "jdbc:sqlserver:/localhost;databaseName=WorksOn;TrustServerCertificate=True";
+	{	String url = "jdbc:sqlserver:/localhost;databaseName=workson;TrustServerCertificate=True";
 		String uid = "sa";
 		String pw = "todo";
 
