439180f90856e9ebe7f5e39d03b893df11c9091f
Ramon Lawrence <ramon.lawrence@ubc.ca>
2023-10-17 13:43:58 -0700

    Update JdbcTransactions.java

--- topics/12_db_programming/code/JdbcTransactions.java
+++ topics/12_db_programming/code/JdbcTransactions.java
@@ -13,7 +13,7 @@ import java.sql.*;
 
 public class JdbcTransactions
 {	public static void main(String[] args)
-	{	String url = "jdbc:sqlserver://localhost;databaseName=WorksOn;TrustServerCertificate=Tru";
+	{	String url = "jdbc:sqlserver://localhost;databaseName=WorksOn;TrustServerCertificate=True";
 		String uid = "sa";
 		String pw = "todo";
 
