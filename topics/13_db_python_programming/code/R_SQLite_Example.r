ee1eb89bef30464325049ead94cbce2ced748d34
Ramon Lawrence <ramon.lawrence@ubc.ca>
2023-10-17 18:10:40 -0700

    Update R_SQLite_Example.r

--- topics/13_db_python_programming/code/R_SQLite_Example.r
+++ topics/13_db_python_programming/code/R_SQLite_Example.r
@@ -5,8 +5,7 @@ library("DBI")
 con <- dbConnect(RSQLite::SQLite(), ":memory:")
 
 # Write data frame to table
-testData <- data.frame(id = 1:5, 
-						name=c("Abe", "Ben", "Cindy", "Dana", "Emma"))
+testData <- data.frame(id = 1:5, name=c("Abe", "Ben", "Cindy", "Dana", "Emma"))
 
 dbWriteTable(con, "test", testData)
 data <- dbGetQuery(con,"SELECT * FROM test")
