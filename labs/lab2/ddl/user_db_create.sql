edbd432f04d2ee3bf317526a790b95cf65cc1982
Ramon Lawrence <ramon.lawrence@ubc.ca>
2026-09-21 20:00:11 -0700

    Update DDL script

--- labs/lab2/ddl/user_db_create.sql
+++ labs/lab2/ddl/user_db_create.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE IF NOT EXISTS mydb;
 
-USE mydb;
-
 USE workson;
 GRANT ALL privileges ON workson.* TO testuser;
 
 USE university;
 GRANT ALL privileges ON university.* TO testuser;
+
+USE shipment;
+GRANT ALL PRIVILEGES ON shipment.* TO testuser;
